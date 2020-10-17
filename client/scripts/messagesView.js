dd0b6f301236c01a06290a658b3f998d9a7f6c8f
Lily Kong <lilyhk28@gmail.com>
2020-10-17 13:04:07 -0400

    implemented the updated messages functions

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,12 +1,34 @@
 var MessagesView = {
 
   $chats: $('#chats'),
-  // first displats before the any message are inputted
+  // first displays before the any message are inputted
+  //MessagesView.$chats.on('submit', MessagesView.handleSumbit)
+
   initialize: function() {
+    MessagesView.render();
   },
+
+  //handleSubmit: function(event) {
+  //event.preventDefault();
+  //need to render all messages in data.results array
+  //}
+
   // updates the messages as more are added
+  render: function() {
+    //iterate through the data.result array and pass each through the renderMessage to render to HTML format, prepend each to the body
+    //console.log('MessagesView.render ran')
+    //console.log('Messages', Messages.data)
+    MessagesView.$chats.html('');
+    _.each(Messages.data, (message) => {
+      if (message.username !== undefined) {
+        MessagesView.renderMessage(message);
+      }
+    });
+  },
+
   renderMessage: function(message) {
-    console.log(message);
+    var $renderedMessage = MessageView.render(message);
+    MessagesView.$chats.append($renderedMessage);
   }
 
 };
